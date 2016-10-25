d109938186c79ed32f8fd924496a5ca0cb880106
Victor Inostroza <vicinost@alumnos.ubiobio.cl>
2016-10-25 11:37:31 -0300

    primer commit con Hola mundo

---
+++ index.php
@@ -0,0 +1,9 @@
+/**
+ * @Author: Victor Inostroza
+ * @Date:   2016-10-25 11:34:58
+ * @Last Modified by:   Victor Inostroza
+ * @Last Modified time: 2016-10-25 11:35:36
+ */
+<?php
+	echo "hola mundo!";
+?>
