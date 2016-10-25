154401d83ca5e87dfce33346e1c36fd5f2eaa743
Victor Inostroza <vicinost@alumnos.ubiobio.cl>
2016-10-25 12:19:13 -0300

    se completa codigo de index

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-	echo "hola mundo!";
-?>
+<doctype/>
+<html>
+	<head>
+
+	</head>
+	<body>
+		<h1><?php
+			echo 'hola mundo!';
+		?>
+		</h1>
+		<?php
+			for($i=1; $i<7;$i++)
+			echo "<h$i>como estas?</h$i>";
+		?>
+	</body>
+</html>
